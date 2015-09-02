96faa3e31cbf643b554a3e38938272ab8c0b30bd
Ziqi Li <liziqi1992@gmail.com>
2015-09-02 12:26:06 -0400

    2.4

--- Ch2. Linked List/2.4.py
+++ Ch2. Linked List/2.4.py
@@ -7,10 +7,56 @@ Input: (3 -> 1 -> 5), (5 -> 9 -> 2)
 Output: 8 -> 0 -> 8
 '''
 
+class node(object):
+    def __init__(self, data,next):
+        self.data = data
+        self.next = next
 
+def printList(list):
+    while list:
+        print list.data
+        list = list.next
 
+def addTwo(a,b):
+    c = node(None,None)
+    list = c
+    temp = 0
+    while a or b:
+        if not a:
+            varA = 0
+            varB = b.data
+        if not b:
+            varB = 0
+            varA = a.data
+        if a and b:
+            varA = a.data
+            varB = b.data
+
+        sum = varA + varB + temp
+        if sum >= 10:
+            temp = 1
+            c.next = node(sum - 10 ,None)
+            c = c.next
+        else:
+            temp = 0
+            c.next = node(sum,None)
+            c = c.next
+        if a:
+            a = a.next
+        if b:
+            b = b.next
+            
+    if temp == 1:
+        c.next = node(1,None)
+    
+    return list.next
 	
 def main():
+    numA = node(9,node(9,node(9,node(9,None))))
+    numB = node(1,None)
+    
+    printList(addTwo(numA,numB))
 
 if __name__ == "__main__":
 	main()
+
